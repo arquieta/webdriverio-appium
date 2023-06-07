0dde5283ca0f5defa72a017857e9c24ba19d582b
Diego Arquieta <arquieta11@gmail.com>
2023-06-07 11:12:45 -0600

    Scrolling updates

--- app/Android/test/specs/android_native.spec copy.js
+++ app/Android/test/specs/android_native.spec copy.js
@@ -33,17 +33,46 @@ describe ('Android native features',  () => {
 
     } )
 
-    it.only('Vertical scrolling', async() => {
+    it('Vertical scrolling', async() => {
         
         await $('~App').click();
         await $('~Activity').click();
-        //scrolling to end. The 1 is how many times to scroll. The 5 is the speed of the scroll
+        //add a pause 
+        await driver.pause(3000);
+        //scrolling to end. The 1 is how many times to scroll. The 5 is the speed of the scroll (not recommended)
         await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(1,5)');
+        //add a pause 
+        await driver.pause(3000);
 
 
         await $('~Secure Surfaces').click();
+        //assertion 
+        await expect($('~Secure Dialog')).toExist();
 
    } )
+   it('Vertical scrolling until element is displayed', async() => {
+        
+    await $('~App').click();
+    await $('~Activity').click();
+    
+    //scrolling to end. The 1 is how many times to scroll. The 5 is the speed of the scroll (not recommended)
+    await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollTextIntoView("Secure Surfaces")').click();
+    
+    //assertion 
+    await expect($('~Secure Dialog')).toExist();
+
+} )
+it.only('Horizontal scrolling until element is displayed', async() => {
+        
+    //access activity
+    await driver.startActivity("io.appium.android.apis", "io.appium.android.apis.view.Gallery1");
+
+    //horizontal scrolling forwards. The 1 is how many times to scroll. The 5 is the speed of the scroll (not recommended)
+    await $('android=new UiScrollable(new UiSelector().scrollable(true)).setAsHorizontalList().scrollForward()').click();
+
+    await driver.pause(3000);
+
+} )
 
 
     
